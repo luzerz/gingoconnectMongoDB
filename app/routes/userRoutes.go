295ff7cfc13a0c262c8c1d35350730f9e552855d
luzerz <amonchaibeer@gmail.com>
2022-09-15 18:18:53 +0700

    Default route

--- app/routes/userRoutes.go
+++ app/routes/userRoutes.go
@@ -1,11 +1,18 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/luzerz/apijobtest/app/controllers"
 )
 
 func UserRoute(router *gin.Engine) {
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Available",
+		})
+	})
 	router.POST("/internal-insert", controllers.CreateUser())
 	router.GET("/internal-read", controllers.GetAUser())
 
